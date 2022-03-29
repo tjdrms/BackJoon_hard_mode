8d8101ad6861b1792996b3e4744803a8577c4041
Seong Keun Choi <84035809+tjdrms@users.noreply.github.com>
2022-03-29 23:18:55 +0900

    백준 5단계(함수) 1065번: 한수
    이 문제 역시 string 변형을 잘이용하여 푸는 문제였습니다. 문자열부분 공부를 많이 해야겠네요..

---
+++ 1065.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+def hansu(n):
+    count=0
+    for i in range(1, n+1):
+        num_list = list(map(int, str(i)))
+        if i < 100:
+            count+=1
+        elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
+            count+=1
+    return count
+
+print(hansu(num))
