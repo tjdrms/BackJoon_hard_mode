2e7896d62607574f429ddf24719ce2a6e5474981
Seong Keun Choi <84035809+tjdrms@users.noreply.github.com>
2022-04-05 00:23:15 +0900

    백준 5622번: 다이얼
    문제에 나온 문자열들을 순서대로 리스트에 저장하여 input값을 토대로 완전탐색을 하는 문제였습니다. 앞의 두 문제와 확실히 난이도 차이를 느꼈습니다.
    이런 것도 못풀면 취직이 가능한걸까 나는..??

---
+++ step6/5622.py
@@ -0,0 +1,10 @@
+dial=['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a=input()
+result = 0
+
+for i in range(len(a)):
+    for j in dial:
+        if a[i] in j:
+            result += dial.index(j)+3
+            
+print(result)
