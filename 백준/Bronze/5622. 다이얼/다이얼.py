2561db0ad89ee49a4c1836e9d878918c95c911bc
Seong Keun Choi <84035809+tjdrms@users.noreply.github.com>
2022-04-05 23:26:14 +0900

    [Bronze II] Title: 다이얼, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,9 @@
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+a = input()
+result = 0
+
+for i in range(len(a)):
+    for j in dial:
+        if a[i] in j:
+            result += dial.index(j)+3
+print(result)
