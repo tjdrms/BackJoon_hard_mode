ae70f14bd1b15da72c3b644ffd236c28c64c5bea
Seong Keun Choi <84035809+tjdrms@users.noreply.github.com>
2022-04-11 22:16:43 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    f0 = [x for x in range(1, n+1)]
+    for i in range(k):
+        for j in range(1, n):
+            f0[j] += f0[j-1]
+    print(f0[-1])
+        
