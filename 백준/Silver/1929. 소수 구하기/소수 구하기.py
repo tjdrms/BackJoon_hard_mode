58569f18091b309a99e24e6d2ddc3b7e73bc6fef
Seong Keun Choi <84035809+tjdrms@users.noreply.github.com>
2022-04-18 22:43:47 +0900

    [Silver III] Title: 소수 구하기, Time: 3660 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+m, n = map(int, input().split())
+
+def isPrime(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num**0.5) + 1):
+            if num%i == 0:
+                return False
+        return True
+    
+for i in range(m, n+1):
+    if isPrime(i):
+        print(i)
